6e6de10e73bdbdef4a19f91e7788f322e59217f9
Daniel Stewardson <84638889+DanielStewardson@users.noreply.github.com>
2022-09-13 14:52:16 +0100

    create Redux Store slice for Topics

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -1,24 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
-
-
-const options = {
-    name: 'topics',
-    initialState: {
-        topics: {}
-    },
-    reducers: {
-        addTopic: (state, action) => {
-            return {
-                ...state,
-                topics: {
-                    ...state.topics,
-                    [action.payload.id]: [action.payload] /* -------- working here ---------- */
-                }
-            }
+const topicsSlice = createSlice({
+  name: "topics",
+  initialState: {
+    topics: {}
+  },
+  reducers: {
+    addTopic: (state, action) => {
+      const { id, name, icon } = action.payload;
+      return {
+        ...state,
+        topics: {
+          ...state.topics,
+          [action.payload.id]: {
+            id: id,
+            name: name,
+            icon: icon,
+            quizIds: []
+          }
         }
+      };
+    },
+    addQuizId: (state, action) => {
+      const { quizId, topicId } = action.payload;
+      state.topics[topicId].quizIds.push(quizId);
     }
-};
+  }
+});
 
-export const topicsSlice = createSlice(options);
+export const selectAllTopics = (state) => state.topics.topics;
+export const { addTopic, addQuizId } = topicsSlice.actions;
+export default topicsSlice.reducer;
