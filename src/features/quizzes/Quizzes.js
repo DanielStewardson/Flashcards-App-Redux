8ca764e9ae407910e796ba6b405f035ba03e7e72
Daniel Stewardson <84638889+DanielStewardson@users.noreply.github.com>
2022-09-13 21:18:06 +0100

    edit css styles and layout of topic and quiz tile lists

--- src/features/quizzes/Quizzes.js
+++ src/features/quizzes/Quizzes.js
@@ -1,23 +1,37 @@
 import { Link } from "react-router-dom";
 import ROUTES from "../../app/routes";
 import { useSelector } from "react-redux";
-import { selectAllQuizzes } from "./quizzezSlice";
+import { selectAllQuizzes } from "./quizzesSlice";
+import { selectAllTopics } from "../topics/topicsSlice";
 
 export default function Quizzes() {
-  const quizzes = useSelector(selectAllQuizzes); // replace this with a call to your selector to get all the quizzes in state
+  const quizzes = useSelector(selectAllQuizzes);
+  const topics = useSelector(selectAllTopics);
+
   return (
     <section className="center">
       <h1>Quizzes</h1>
-      <ul className="quizzes-list">
-        {Object.values(quizzes).map((quiz) => (
-          <Link key={quiz.id} to={ROUTES.quizRoute(quiz.id)}>
-            <li className="quiz">{quiz.name}</li>
-          </Link>
-        ))}
-      </ul>
       <Link to={ROUTES.newQuizRoute()} className="button">
         Create New Quiz
       </Link>
+      <ul className="big-tile-list">
+        {Object.values(quizzes).map((quiz) => (
+          <Link to={ROUTES.quizRoute(quiz.id)} key={quiz.id}>
+            <li className="big-tile quiz" key={quiz.id}>
+              <div className="tile-text-container">
+                <img src={topics[quiz.topicId].icon} alt="" />
+                <div className="text-content">
+                  <h2>{quiz.name}</h2>
+                  <div className='quiz-tile-sub-text'>
+                    <p style={{paddingRight: '20px'}}>Topic: {topics[quiz.topicId].name}</p>
+                    <p>Cards: {quiz.cardIds.length}</p>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </Link>
+        ))}
+      </ul>
     </section>
   );
 }
