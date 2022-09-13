6d07b400026f66b0939028af52e55e5f6e417383
Daniel Stewardson <84638889+DanielStewardson@users.noreply.github.com>
2022-09-13 14:53:31 +0100

    Import store slices into store file

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import topicsReducer from "../features/topics/topicsSlice";
+import quizzezReducer from "../features/quizzes/quizzezSlice";
+import cardsReducer from "../features/cards/cardsSlice";
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    topics: topicsReducer,
+    quizzes: quizzezReducer,
+    cards: cardsReducer
+  },
 });
