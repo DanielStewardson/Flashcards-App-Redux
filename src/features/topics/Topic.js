437d81ae1814abc4e5fb89b5980e19f234129c18
Daniel Stewardson <84638889+DanielStewardson@users.noreply.github.com>
2022-09-21 11:54:04 +0100

    change layout of topic page and add delete quiz button to quiz tiles

--- src/features/topics/Topic.js
+++ src/features/topics/Topic.js
@@ -1,9 +1,9 @@
 import NewTopicForm from "../../components/NewTopicForm";
 import { Link, useParams } from "react-router-dom";
 import ROUTES from "../../app/routes";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { selectAllTopics } from "./topicsSlice";
-import { selectAllQuizzes } from "../quizzes/quizzesSlice";
+import { selectAllQuizzes, deleteQuizThunk } from "../quizzes/quizzesSlice";
 
 export default function Topic() {
   const topics = useSelector(selectAllTopics);
@@ -12,20 +12,46 @@ export default function Topic() {
   const topic = topics[topicId];
   const quizzesForTopic = topic.quizIds.map((quizId) => quizzes[quizId]);
 
+  const dispatch = useDispatch();
+
+  const deleteQuiz = (e, quizId, topicId) => {
+    e.preventDefault();
+    dispatch(deleteQuizThunk(quizId, topicId))
+  };
+
+  const buttonPosition = Object.values(quizzesForTopic).length === 0 ? 'center' : 'right-side-button';
+
   return (
     <section>
       <img src={topic.icon} alt="" className="topic-icon" />
       <h1>Topic: {topic.name}</h1>
-      <ul className="quizzes-list">
-        {quizzesForTopic.map((quiz) => (
-          <li className="quiz" key={quiz.id}>
-            <Link to={ROUTES.quizRoute(quiz.id)}>{quiz.name}</Link>
-          </li>
-        ))}
-      </ul>
-      <Link to="/quizzes/new" className="button center">
+      <Link to="/quizzes/new" className={`button ${buttonPosition}`}>
         Create a New Quiz
       </Link>
+      <ul className="big-tile-list">
+        {quizzesForTopic.map((quiz) => (
+          <Link to={ROUTES.quizRoute(quiz.id)} key={quiz.id}>
+            <li className="big-tile quiz" key={quiz.id}>
+              <button 
+                className='delete-quiz' 
+                value={quiz.id} 
+                onClick={(e) => deleteQuiz(e, quiz.id, quiz.topicId)}>
+                X
+              </button>
+              
+              <div className="tile-text-container">
+                <div className="text-content">
+                  <h2>{quiz.name}</h2>
+                  <div className='quiz-tile-sub-text'>
+                    <p>Cards: {quiz.cardIds.length}</p>
+                  </div>
+                </div>
+              </div>
+
+            </li>
+          </Link>
+        ))}
+      </ul>
     </section>
   );
 }
