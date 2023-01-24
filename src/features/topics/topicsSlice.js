1b1d23a79fccc7e31c2566e586c4dc2b235352f7
Jacob Middleton <109218193+JacobMiddleton@users.noreply.github.com>
2023-01-24 18:10:58 -0500

    Add comments and refactoring

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -16,46 +16,37 @@ const topicsSlice = createSlice({
     topics: {}
   },
   reducers: {
+    // We can refactor these action creators
+    // createSlice uses immer, which allows you to update the state using 'mutating' syntax
+    // We therefore don't need to use the spread syntax (...)
+    // To learn more visit: https://redux-toolkit.js.org/usage/immer-reducers
     addTopic: (state, action) => {
       const { id, name, icon } = action.payload;
-      return {
-        ...state,
-        topics: {
-          ...state.topics,
-          [action.payload.id]: {
+          state.topics[action.payload.id] = {
             id: id,
             name: name,
             icon: icon,
             quizIds: []
           }
-        }
-      };
     },
-    deleteTopic: (state, action) => {
-      const newTopics = Object.fromEntries(Object.entries(state.topics).filter(([key]) => key !== action.payload));
-      return {
-        ...state,
-        topics: {
-          ...newTopics
-        }
-      }
+     deleteTopic: (state, action) => {
+       const newTopics = Object.fromEntries(Object.entries(state.topics).filter(([key]) => key !== action.payload));
+       state.topics = newTopics;
     },
     addQuizId: (state, action) => {
       const { quizId, topicId } = action.payload;
       state.topics[topicId].quizIds.push(quizId);
     },
+
     removeQuizId: (state, action) => {
       const { quizId, topicId } = action.payload;
-      return {
-        ...state,
-        topics: {
-          ...state.topics,
+        state.topics = {
           [action.payload.topicId]: {
+    // had trouble refactoring these two lines
             ...state.topics[action.payload.topicId],
             quizIds: [...state.topics[topicId].quizIds.filter(id => id !== quizId)]
           }
         }
-      };
     }
   }
 });
